fa5b4cf7083981fc973db9705ee1320ed65ea4ea
leonardoarroyo <git@leonardoarroyo.com>
2022-02-10 21:15:13 -0300

    :-)

--- configuration.nix
+++ configuration.nix
@@ -17,6 +17,9 @@ in
       (import "${home-manager}/nixos")
     ];
 
+  # Nvidia
+  # services.xserver.videoDrivers = [ "nvidia" ];
+
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
@@ -48,8 +51,12 @@ in
     pulse.enable = true;
   };
 
+  # Flatpak
+  services.flatpak.enable = true;
+
   # Enable bluetooth
   hardware.bluetooth.enable = true;
+  xdg.portal.enable = true;
   
   # Non-root user
   users.extraUsers.arroyo = {
@@ -68,6 +75,10 @@ in
 
   # Enable docker
   virtualisation.docker.enable = true;
+
+  # Enable virtualbox
+  virtualisation.virtualbox.host.enable = true;
+  users.extraGroups.vboxusers.members = [ "arroyo" ];
   
   # Xorg
   services.xserver = {
@@ -94,9 +105,19 @@ in
     };
   };
 
+  # GNUPG
+  services.pcscd.enable = true;
+  programs.gnupg.agent = {
+     enable = true;
+     pinentryFlavor = "curses";
+     enableSSHSupport = true;
+  };
 
   # System packages
   environment.systemPackages = with pkgs; [
+    gnupg
+    pinentry
+    pinentry-curses
     postgresql
     python-with-my-packages
     wget
@@ -111,10 +132,25 @@ in
     bitwarden
     bitwarden-cli
     nodejs
-    whatsapp-for-linux
-    # stack
+    stack
+    evince
+    okular
+    xournal
+    gimp
+    hakuneko
+    qbittorrent
+    qimgv
+    scrot
+    calibre
+    slack
+    zoom-us
+    teams
+    evolution
+    git-crypt
+    chromium
   ];
 
+  programs.steam.enable = true;
   # Arroyo programs
   home-manager.users.arroyo = {
     programs = {
@@ -130,18 +166,26 @@ in
 
       vscode = {
         enable = true;
-        package = pkgs.vscodium;
+        package = pkgs.vscode;
         extensions = with pkgs.vscode-extensions; [
           haskell.haskell
           justusadam.language-haskell
           jnoortheen.nix-ide
           asvetliakov.vscode-neovim
+	  eamodio.gitlens
+          ms-vscode-remote.remote-ssh
         ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
           {
             "name" = "monokai-charcoal-high-contrast";
             "publisher" = "74th";
             "version" = "3.4.0";
             "sha256" = "2ec51074ee2052e065b4ef2a04e9e186edbde4e3b7d59eca4cbec78a316fc817";
+          }
+          {
+            "name" = "github-markdown-preview";
+            "publisher" = "bierner";
+            "version" = "0.1.0";
+            "sha256" = "8c7b0d454a2d95e7a1f28265d98a1a84039b818b95b05c227fb21952c3f44a4f";
           }
         ];
       };
@@ -159,6 +203,7 @@ in
             co = "checkout";
             ci = "commit";
             st = "status";
+	    lp = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
           };
         };
       };
@@ -170,6 +215,11 @@ in
       rofi = {
         enable = true;
       };
+
+      # whatsappForLinux = {
+      # 	enable = true;
+      #   package = pkgs.whatsapp-for-linux;
+      # };
     };
   };
 
@@ -178,6 +228,9 @@ in
     binaryCaches          = [ "https://hydra.iohk.io" "https://iohk.cachix.org" ];
     binaryCachePublicKeys = [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo=" ];
   };
+
+  # Allow unfree
+  nixpkgs.config.allowUnfree = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
